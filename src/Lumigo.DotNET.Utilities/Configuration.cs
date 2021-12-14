292f22dd8866c795b38155a1a27a3cd47d24fb8c
CircleCI <no-reply@build.com>
2021-12-14 12:07:53 +0000

    1.0.29 → 1.0.30. Changes:

--- src/Lumigo.DotNET.Utilities/Configuration.cs
+++ src/Lumigo.DotNET.Utilities/Configuration.cs
@@ -51,7 +51,7 @@ namespace Lumigo.DotNET.Utilities
 
         public string GetLumigoTracerVersion()
         {
-            return "1.0.29";  // Being updated from .bumpversion.cfg
+            return "1.0.30";  // Being updated from .bumpversion.cfg
         }
 
         public bool DebugMode
