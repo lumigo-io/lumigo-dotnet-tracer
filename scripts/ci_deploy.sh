9e324ff02c39161a35b489703fe5a32c2b34847e
nirLumigo <nir@lumigo.io>
2021-12-14 14:55:08 +0200

    rename project

--- scripts/ci_deploy.sh
+++ scripts/ci_deploy.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 pushd "$(dirname "$0")" &> /dev/null
 # Go back one spot because we are in scripts dir. The other scripts assume you are in the root folder
 cd ..
-../utils/common_bash/defaults/ci_deploy.sh dotnet-tracer
+../utils/common_bash/defaults/ci_deploy.sh lumigo-dotnet-tracer
 popd &> /dev/null
