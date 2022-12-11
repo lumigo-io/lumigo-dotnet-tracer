5f36d741c63a0c8006ccda40892142903f48fb47
Adam Fisher <adamf@lumigo.io>
2022-12-11 18:01:55 +0200

    chore: migrate to new workflow

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
 set -e
-pushd test/Lumigo.DotNET.Test/ 2>&1
-dotnet add package coverlet.collector
-dotnet test --collect:"XPlat Code Coverage"
-popd 2>&1
+
+pushd test/Lumigo.DotNET.Test/
+    dotnet add package coverlet.collector
+    dotnet test --collect:"XPlat Code Coverage"
+popd
+
 ../utils/common_bash/defaults/code_cov.sh
