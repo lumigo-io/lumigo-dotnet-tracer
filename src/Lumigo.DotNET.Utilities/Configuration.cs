1d2cf4ae497a537b4c2f2d48bee3e6f9a9c01653
CircleCI <no-reply@build.com>
2022-12-15 16:40:12 +0000

    1.0.42 → 1.0.43. Changes: 8ae03e6 Merge pull request #16 from lumigo-io/RD-9853-fix-missing-error-spans 02374b7 fix: correctly mark failed invocations

--- src/Lumigo.DotNET.Utilities/Configuration.cs
+++ src/Lumigo.DotNET.Utilities/Configuration.cs
@@ -57,7 +57,7 @@ namespace Lumigo.DotNET.Utilities
 
         public string GetLumigoTracerVersion()
         {
-            return "1.0.42";  // Being updated from .bumpversion.cfg
+            return "1.0.43";  // Being updated from .bumpversion.cfg
         }
 
         public bool DebugMode
