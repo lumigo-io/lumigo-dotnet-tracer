665c65a672266e459dce965a2b4b29eaa74bd592
Guy Moses <guy@lumigo.io>
2024-09-09 10:12:05 +0300

    imports

--- src/Lumigo.DotNET.Instrumentation/Handlers/Empty/EmptyHandler.cs
+++ src/Lumigo.DotNET.Instrumentation/Handlers/Empty/EmptyHandler.cs
@@ -8,33 +8,10 @@ namespace Lumigo.DotNET.Instrumentation.Handlers.Empty
 {
     public class EmptyHandler : BaseServiceHandler
     {
-        private JsonSerializerSettings _jsonSerializerSettings;
-
-        // Property with lazy initialization
-        private JsonSerializerSettings JsonSerializerSettings
+        JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings
         {
-            get
-            {
-                if (_jsonSerializerSettings == null)
-                {
-                    _jsonSerializerSettings = new JsonSerializerSettings
-                    {
-                        ContractResolver = new DynamicIgnoreResolver(),
-                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                        Error = HandleSerializationError
-                    };
-                }
-                return _jsonSerializerSettings;
-            }
-        }
-
-        private void HandleSerializationError(object sender, ErrorEventArgs args)
-        {
-            var currentError = args.ErrorContext.Error.Message;
-            Logger.LogDebug("Failed to Serialize JSON" + currentError);
-
-            // Ignore the error and continue serialization
-            args.ErrorContext.Handled = true;
+            ContractResolver = new IgnoreStreamsResolver(),
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
         }
 
         private string _serviceName;
